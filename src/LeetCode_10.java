e4590a513fbdd1aa961ffd5de8490471acde8e73
zhangzc <347328331@qq.com>
2017-08-11 16:36:44 +0800

    leetcode第10题

--- src/LeetCode_10.java
+++ src/LeetCode_10.java
@@ -1,37 +1,37 @@
+/**
+ *  简单正则表达式引擎
+ *  '.'代表匹配任意字符
+ *  '*'代表匹配前面的字符0个或任意个
+ */
 public class LeetCode_10 {
 
     public boolean isMatch(String s, String p) {
-        int slen = s.length();
-        int plen = p.length();
-        int i = 0 ,j = 0;
-        while (i<slen&&j<plen){
-            char sc = s.charAt(i);
-            char pc = p.charAt(j);
-            if (sc == pc ||pc == '.'){
-                i++;j++;
-            }else if (pc == '*'){
-                if ((j-1 > 0)&&(sc == p.charAt(j-1)||p.charAt(j-1) == '.')){
-                    i++;
-                }else {
-                    j++;
-                }
-            }else if ((++j<plen )&& p.charAt(j) == '*'){
-                j++;
+        return match(s,p,0,0);
+    }
+    public boolean match(String s, String p,int i,int j) {
+        boolean ret = false;
+        if (i==s.length()&&j==p.length()){
+            return true;
+        }
+        if ((j+1)<p.length()&&p.charAt(j+1)=='*'){
+            ret = match(s,p,i,j+2);
+        }
+        if (i>=s.length()||j >= p.length()){
+            return ret;
+        }
+        if (s.charAt(i) == p.charAt(j)||p.charAt(j) == '.'){
+            if ((j+1)<p.length()&&p.charAt(j+1)=='*'){
+                ret = ret || match(s, p, i + 1, j);
             }else {
-                return false;
+                ret =  ret||match(s,p,i+1,j+1);
             }
+        }else {
+            return ret;
         }
-        if (i<slen){
-            return false;
-        }
-        while (j<plen){
-
-        }
-
-        return true;
+        return ret;
     }
     public static void main(String[] args) {
         LeetCode_10 ll = new LeetCode_10();
-        System.out.println(ll.isMatch("asdasd","a.*d"));
+        System.out.println(ll.isMatch("aa","aaa"));
     }
 }
