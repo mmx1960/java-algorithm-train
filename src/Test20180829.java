9cf8ff7c6d6a90b488abff82eb882a7275ed9a23
fengche <zhangzch@uuzu.com>
2018-08-30 14:38:16 +0800

    算法完善

--- src/Test20180829.java
+++ src/Test20180829.java
@@ -4,7 +4,6 @@
 public class Test20180829 {
     public static String[][] findCollection(String[] input,int num){
         int length = input.length;
-
         //计算数组大小
         int p = 1;
         int c = 1;
@@ -17,29 +16,36 @@ public class Test20180829 {
         //初始化结果数组
         String[][] child = new String[total][num];
         //构建结果
-        for(int i = 0; i+num < length;i++){
-            start = calc(input,child,num,start,length,i,0);
+        for(int i = 0; i+num <= length;i++){
+            start = calc(input,child,num,start,length,i,0,null,0);
         }
         return child;
     }
-    public static int calc(String[] input,String[][] child,int num,int start,int length,int i,int current){
+    public static int calc(String[] input,String[][] child,int num,int start,int length,int i,int current,String[] last,int len){
         int second = 1;
         int first = num - second;
         while (first > 0){
-            for (int j = i+current+first ;j+second < length;j++){
+            String[] curr = new String[len + first];
+            if (last != null){
+                System.arraycopy(last, 0 , curr, 0, len);
+            }
+            if (current == 0){
+                current+=i;
+            }
+            System.arraycopy(input,current , curr, len, first);
+            int currLen = len + first;
+            for (int j= current+first,k = current+first;j+second <= length;j++,k++){
                 if (second > 1){
-                    start = calc(input,child,second,start,length,i,current+first);
-                }else {
-                    if (current != 0){
-                        System.arraycopy(input, i, child[start], 0, current);
-                        System.arraycopy(input, i + current + 1, child[start], current, first);
-                        System.arraycopy(input, j + 1, child[start], current+first, second);
-                    }else {
-                        System.arraycopy(input, i , child[start], current, first);
-                        System.arraycopy(input, j, child[start], current+first, second);
+                    if (j == k){
+                        j++;
                     }
+                    start = calc(input,child,second,start,length,i,j,curr,currLen);
+                }else {
+                    System.arraycopy(curr, 0, child[start], 0, currLen);
+                    System.arraycopy(input, j, child[start], currLen, second);
                     start++;
                 }
+
             }
             first--;
             second++;
@@ -49,7 +55,7 @@ public class Test20180829 {
 
     public static void main(String[] args) {
 
-        String[][] collection = findCollection(new String[]{"a", "b", "c", "d" ,"e"}, 3);
+        String[][] collection = findCollection(new String[]{"a", "b", "c", "d" ,"e","f"}, 3);
         //拼装结果
         StringBuilder sb = new StringBuilder();
         sb.append("{");
